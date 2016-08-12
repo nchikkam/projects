765a1ba35b64918794963416f0f3a5a261f49acc
nchikkam <narayana.chikkam@gmail.com>
2016-08-13 01:54:51 +0530

    organizing the learning.

---
+++ fp/ml/structure/average.ml
@@ -0,0 +1,5 @@
+(* let ... in *)
+let average a b = 
+	let sum = a +. b in sum /. 2.0;;             (* ;; ends the sum definition block*)
+
+let () = print_string (string_of_float (average 2.0 4.0))
