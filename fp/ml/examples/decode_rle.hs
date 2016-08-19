812de0abd1c49442491ad9edf135dea62baeaee3
nchikkam <narayana.chikkam@gmail.com>
2016-08-20 00:01:52 +0530

    reverse of rle.

---
+++ fp/ml/examples/decode_rle.hs
@@ -0,0 +1,10 @@
+let decode list =
+    let rec many acc n x =
+      if n = 0 then acc else many (x :: acc) (n-1) x in
+    let rec aux acc = function
+      | [] -> acc
+      | One x :: t -> aux (x :: acc) t
+      | Many (n,x) :: t -> aux (many acc n x) t  in
+    aux [] (List.rev list);;
+
+decode [Many (4,"a"); One "b"; Many (2,"c"); Many (2,"a"); One "d"; Many (4,"e")];;
