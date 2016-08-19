b7b9f3fc29a7023427112dec998154739f10b146
nchikkam <narayana.chikkam@gmail.com>
2016-08-19 22:50:00 +0530

    run length encoding or counting sort.

---
+++ fp/ml/examples/run_lenth_encode.hs
@@ -0,0 +1,9 @@
+let encode list =
+    let rec aux count acc = function
+      | [] -> [] (* Can only be reached if original list is empty *)
+      | [x] -> (count+1, x) :: acc
+      | a :: (b :: _ as t) -> if a = b then aux (count + 1) acc t
+                              else aux 0 ((count+1,a) :: acc) t in
+    List.rev (aux 0 [] list);;
+
+encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];;
