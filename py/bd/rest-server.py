c95606aac2bd195cf3f86bcabad1e05cb7b2e642
nchikkam <narayana.chikkam@gmail.com>
2016-08-26 15:38:56 +0530

    removing stale files.

--- py/bd/rest-server.py
+++
@@ -1,9 +0,0 @@
-
-from app.app import app
-#from flask.ext.sqlalchemy import SQLAlchemy
-
-if __name__ == '__main__':
-#    app.config.from_object('config')
-#    db = SQLAlchemy(app)
-
-    app.run(debug=True)
