bbc90733aecba4e4b87ae8b531457eea1cf6ef7c
nchikkam <narayana.chikkam@gmail.com>
2016-09-13 23:59:36 +0530

    9 from 5.

--- py/dice_simulation.py
+++ py/dice_simulation.py
@@ -7,7 +7,11 @@ def dice7():
     r = dice5() + dice5() * 5 - 6
     return (r % 7) + 1 if r < 21 else dice7()
 
+
 # proof
 for i in range(1, 6):
     for j in range(1, 6):
         print(i*5 + j -5)
+
+def dice12():
+    pass
